592f8951770dfc3089a41b1fae23e73756ec0027
Deepak345 <senapati.dipak97@gmail.com>
2017-10-29 04:35:04 +0530

    updated the UI for the organisation's pages

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -9,7 +9,6 @@
  * Main module of the application.
  */
 angular
-<<<<<<< HEAD
 
     .module('clientApp', [
         'ngRoute',
@@ -57,6 +56,11 @@ angular
                 controller: 'OrgsignupCtrl',
                 controllerAs: 'orgsignup'
             })
+            .when('/sendmails', {
+                templateUrl: 'views/sendmails.html',
+                controller: 'SendmailCtrl',
+                controllerAs: 'sendmail'
+            })
             .when('/banksignup', {
                 templateUrl: 'views/banksignup.html',
                 controller: 'BanksignupCtrl',
@@ -91,90 +95,3 @@ angular
                 redirectTo: '/'
             });
     });
-=======
-  .module('clientApp', [
-    'ngRoute',
-    'ngSanitize'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/upcomingevents', {
-        templateUrl: 'views/upcomingevents.html',
-        controller: 'UpcomingeventCtrl',
-        controllerAs: 'upev'
-      })
-      .when('/proximalbanks', {
-        templateUrl: 'views/proximalbanks.html',
-        controller: 'ProximalCtrl',
-        controllerAs: 'proxcamp'
-      })
-      .when('/proximalbanksdetails', {
-        templateUrl: 'views/proximalbanksdetail.html',
-        controller: 'ProximaldetCtrl',
-        controllerAs: 'proxdetcamp'
-      })
-      .when('/useroptions', {
-        templateUrl: 'views/useroptions.html',
-        controller: 'UseropCtrl',
-        controllerAs: 'userop'
-      })
-      .when('/signup', {
-        templateUrl: 'views/signup.html',
-        controller: 'SignupCtrl',
-        controllerAs: 'signup'
-      })
-      .when('/organizationsignup', {
-        templateUrl: 'views/organizationsignup.html',
-        controller: 'OrgsignupCtrl',
-        controllerAs: 'orgsignup'
-      })
-      .when('/sendmails', {
-        templateUrl: 'views/sendmails.html',
-        controller: 'SendmailCtrl',
-        controllerAs: 'sendmail'
-      })
-      .when('/banksignup', {
-        templateUrl: 'views/banksignup.html',
-        controller: 'BanksignupCtrl',
-        controllerAs: 'banksignup'
-      })
-      .when('/regforbank', {
-        templateUrl: 'views/regforbank.html',
-        controller: 'BankregCtrl',
-        controllerAs: 'bankreg'
-      })
-      .when('/regfororg', {
-        templateUrl: 'views/regfororg.html',
-        controller: 'OrgregCtrl',
-        controllerAs: 'orgreg'
-      })
-      .when('/sharedlink/:id', {
-        templateUrl: 'views/sharedlink.html',
-        controller: 'LinkCtrl',
-        controllerAs: 'link'
-      })
-      .when('/orgdashboard', {
-        templateUrl: 'views/orgdashboard.html',
-        controller: 'OrgdashCtrl',
-        controllerAs: 'orgdash'
-      })
-      .when('/createcamp', {
-        templateUrl: 'views/createcamp.html',
-        controller: 'CreatecampCtrl',
-        controllerAs: 'createcamp'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
->>>>>>> 89b8b5b256d97da54f5906062963d3484dcb7317
