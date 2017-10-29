a8d7394784828216b6e608cb61c42d3d9b81b0d7
Deepak345 <senapati.dipak97@gmail.com>
2017-10-29 11:17:29 +0530

    dpk

--- client/app/scripts/controllers/upcomingevents.js
+++ client/app/scripts/controllers/upcomingevents.js
@@ -1,28 +1,29 @@
 'use strict';
 
 angular.module('clientApp')
-.controller('UpcomingeventCtrl', function ($scope, $http) {
-  $scope.donor = {
-    bloodGrp: "A+ve",
-    gender: "Male"
-  };
+    .controller('UpcomingeventCtrl', function($scope, $http, $location) {
+        $scope.donor = {
+            bloodGrp: "A+ve",
+            gender: "Male"
+        };
 
-  this.text = "upcoming event works" ;
+        this.text = "upcoming event works";
 
-  $http.get('/getallevents').then(function(res) {
-    console.log(res);
-    $scope.events = res.data;
-  }); 
+        $http.get('/getallevents').then(function(res) {
+            console.log(res);
+            $scope.events = res.data;
+        });
 
-  $scope.saveEvent = function(id) { 
-    console.log(id);
-    $scope.eventId = id;
-  };
+        $scope.saveEvent = function(id) {
+            console.log(id);
+            $scope.eventId = id;
+        };
 
-  $scope.register = function() {
-    console.log($scope.eventId);
-    $http.post('/registertoevent', { eventId: $scope.eventId, donor: $scope.donor }).then(function(res) {
-      console.log(res);
+        $scope.register = function() {
+            console.log($scope.eventId);
+            $http.post('/registertoevent', { eventId: $scope.eventId, donor: $scope.donor }).then(function(res) {
+                console.log(res);
+                $location.path("upcomingevents");
+            });
+        };
     });
-  };
-});
