66baed10b92c82434304932ca8f5c71270c192bc
N V S Abhishek <nvsabhishek.india@gmail.com>
2017-11-20 15:43:53 +0530

    sdfdsf

--- client/app/scripts/controllers/banksignup.js
+++ client/app/scripts/controllers/banksignup.js
@@ -1,8 +1,42 @@
 'use strict';
 
 angular.module('clientApp')
-.controller('BanksignupCtrl', function ($rootScope , userservice) {
-  this.text = "bank sign up works" ;
-  $rootScope.color = 'blue';
-  console.log($rootScope.color);
+.controller('BanksignupCtrl', function ($scope) {
+  $scope.regForm = {
+    name : "",
+    contact : "",
+    email : "",
+    city : "",
+    district : "",
+    state : "",
+    category : "Public",
+    pincode : "",
+    nodalOfficerName : "",
+    nodalOfficerNo : "",
+    nodalOfficerEmail : ""
+  };
+
+  $scope.checkEmptyFields = function() {
+    for (var prop in $scope.regForm) {
+      if ($scope.regForm.hasOwnProperty(prop)) {
+          if($scope.regForm[prop] === "") {
+            return false;
+          }
+      }
+    }
+    return true;
+  };
+
+  $scope.submit = function() {
+    if($scope.checkEmptyFields()) {
+      $http.post('/registerbank', regForm)
+            .then(function(res) {
+              console.log(res);
+              alert("Registration Successful!");
+            });
+      } else {
+        alert("One or More Fields Empty!!");
+      }
+  }
+
 });
